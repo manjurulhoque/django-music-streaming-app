28a6c53cbde570de4ac6925811e2139172c7d961
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-03-29 22:36:38 +0600

    Genre serializer done

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Song, Artist
+from core.models import Song, Artist, Genre
 
 
 class ArtistSerializer(serializers.ModelSerializer):
@@ -9,8 +9,15 @@ class ArtistSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
 class SongSerializer(serializers.ModelSerializer):
     artists = ArtistSerializer(many=True)
+    genre = GenreSerializer()
 
     class Meta:
         model = Song
